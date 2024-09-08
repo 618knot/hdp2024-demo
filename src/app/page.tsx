cbc864dfb9caff2804065ad1fc43eb97b7b9b1f4
618knot <knot618@gmail.com>
2024-09-08 16:13:37 +0900

    タイトル追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ModeSelectContainer from "./_components/ModeSelectContainer";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-xl font-bold my-8">ハードウェアデザインプロジェクト DEMO</h1>
       <ModeSelectContainer />
     </main>
   );
