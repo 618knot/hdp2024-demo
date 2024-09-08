c19dda66c5a6e4c4a7579a94b3febbae367b350a
618knot <knot618@gmail.com>
2024-09-08 16:15:18 +0900

    shadcn系対応・メニューバー・ダークモード

---
+++ src/app/@menubar/page.tsx
@@ -0,0 +1,33 @@
+import { contents } from "@/components/contents"
+import { ModeToggle } from "@/components/ModeToggle"
+import { Button } from "@/components/ui/button"
+import {
+  Menubar,
+  MenubarMenu,
+} from "@/components/ui/menubar"
+import { Home } from "lucide-react"
+import Link from "next/link"
+ 
+export default function MenubarDemo() {
+  return (
+    <Menubar className="absolute m-3">
+      <MenubarMenu>
+        <Button variant="link" asChild>
+          <Link href="/">HOME <Home className="ml-0.5 opacity-60" /></Link>
+        </Button>
+        {
+          contents.map((content, index) => {
+            return(
+            <Button key={index} variant="link" asChild>
+              <Link href={content.path}>
+                {content.content}
+              </Link>
+            </Button>
+            )
+          })
+        }
+        <ModeToggle />
+      </MenubarMenu>
+    </Menubar>
+  )
+}
