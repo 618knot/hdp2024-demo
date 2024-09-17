abe9689c584c369b89ad724506c0f082415530a7
618knot <publicac.clt618knot@gmail.com>
2024-09-17 18:23:47 +0900

    途中経過

--- src/app/bluetooth/page.tsx
+++ src/app/bluetooth/page.tsx
@@ -6,19 +6,53 @@ import { NextReactP5Wrapper } from "@p5-wrapper/next";
 import sketch from "@/p5/sketch.js";
 import { useState } from "react";
 import Title from "@/components/Title";
-import { contents } from "@/components/contents";
-import { Bluetooth } from "lucide-react";
+import { Bluetooth, Flame } from "lucide-react";
+import { toast } from "sonner";
+
+let intervalID: NodeJS.Timeout;
 
 export default function Home() {
   const [xVal, setXVal] = useState<number>(0);
+  const [isPending, setIsPending] = useState<boolean>(false);
 
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <Title>Bluetooth<Bluetooth className="ml-0.5 opacity-60" /></Title>
-      <Button onClick={() => {
-        const dev = requestDevice();
-        console.log(dev);
-      }}>
+      <Button onClick={async () => {
+        const device = await requestDevice({
+          filters: [
+            { services: ["12345678-1234-5678-1234-56789abcdef0"] }
+          ],
+          optionalServices: ["12345678-1234-5678-1234-56789abcdef0"]
+        });
+        setIsPending(true);
+        await device.gatt.connect();
+        console.log(isPending)
+        // if (!device.gatt.connected) {
+        //   toast("デバイスとの接続に失敗しました" ,{
+        //     icon: <Flame />,
+        //     style: { background: "#dc2626", color: "#fff" },
+        //   });
+        //   isPending = false;
+        //   return;
+        // }
+        const primary = await device.gatt.getPrimaryService("12345678-1234-5678-1234-56789abcdef0");
+        const characteristic = await primary.getCharacteristic("12345678-1234-5678-1234-56789abcdef1");
+        if(!intervalID) {
+          intervalID = setInterval(
+            async () => {
+              const value: DataView = await characteristic.readValue();
+          
+              const decoder = new TextDecoder("utf-8");
+              const decodedJson = JSON.parse(decoder.decode(value))
+              console.log(decodedJson)
+            },
+            500
+          );
+        }
+      }}
+      disabled={isPending}
+      >
         ジョイスティックと接続する(予定)
       </Button>
       <p>wasdか十字キーでも操作できます</p>
