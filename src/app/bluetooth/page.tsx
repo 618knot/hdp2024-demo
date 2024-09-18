5f6666eb3fda0fd4cab0dfa1265b7d770f4dfd9a
618knot <publicac.clt618knot@gmail.com>
2024-09-18 13:21:31 +0900

    uuidの設定や接続のハンドリング

--- src/app/bluetooth/page.tsx
+++ src/app/bluetooth/page.tsx
@@ -7,54 +7,71 @@ import sketch from "@/p5/sketch.js";
 import { useState } from "react";
 import Title from "@/components/Title";
 import { Bluetooth, Flame } from "lucide-react";
-import { toast } from "sonner";
+
+const SERVICE_UUID = '96a3093b-708c-4abd-97d2-9d8b10c122ec';
+const CHAR_UUID = '206ff6bf-5f3e-4c9e-902f-b7762595ddd8';
+const NAME_PREFIX = "hdp2024-";
 
 let intervalID: NodeJS.Timeout;
+let device: any;
 
 export default function Home() {
   const [xVal, setXVal] = useState<number>(0);
   const [isPending, setIsPending] = useState<boolean>(false);
+  const [wasntSettedInterval, setWasntSettedInterval] = useState<boolean>(true);
 
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <Title>Bluetooth<Bluetooth className="ml-0.5 opacity-60" /></Title>
-      <Button onClick={async () => {
-        const device = await requestDevice({
-          filters: [
-            { services: ["12345678-1234-5678-1234-56789abcdef0"] }
-          ],
-          optionalServices: ["12345678-1234-5678-1234-56789abcdef0"]
-        });
-        setIsPending(true);
-        await device.gatt.connect();
-        console.log(isPending)
-        // if (!device.gatt.connected) {
-        //   toast("デバイスとの接続に失敗しました" ,{
-        //     icon: <Flame />,
-        //     style: { background: "#dc2626", color: "#fff" },
-        //   });
-        //   isPending = false;
-        //   return;
-        // }
-        const primary = await device.gatt.getPrimaryService("12345678-1234-5678-1234-56789abcdef0");
-        const characteristic = await primary.getCharacteristic("12345678-1234-5678-1234-56789abcdef1");
-        if(!intervalID) {
-          intervalID = setInterval(
-            async () => {
-              const value: DataView = await characteristic.readValue();
-          
-              const decoder = new TextDecoder("utf-8");
-              const decodedJson = JSON.parse(decoder.decode(value))
-              console.log(decodedJson)
-            },
-            500
-          );
-        }
-      }}
-      disabled={isPending}
-      >
-        ジョイスティックと接続する(予定)
-      </Button>
+      {
+        isPending
+        ? <Button onClick={() => {
+          clearInterval(intervalID);
+          device.gatt.disconnect();
+          setWasntSettedInterval(true);
+          setIsPending(false);
+        }}
+        disabled={wasntSettedInterval}
+        >
+            接続を停止
+          </Button>
+        : <Button onClick={async () => {
+          device = await requestDevice({
+            filters: [
+              { services: [SERVICE_UUID] },
+              { namePrefix: NAME_PREFIX }
+            ],
+            optionalServices: [SERVICE_UUID]
+          });
+          await setIsPending(true);
+          await device.gatt.connect();
+          await console.log(isPending)
+          const primary = await device.gatt.getPrimaryService(SERVICE_UUID);
+          const characteristic = await primary.getCharacteristic(CHAR_UUID);
+          if(!intervalID) {
+            intervalID = await setInterval(
+              async () => {
+                try {
+                  const value: DataView = await characteristic.readValue();
+            
+                  const decoder = new TextDecoder("utf-8");
+                  const decodedJson = JSON.parse(decoder.decode(value))
+                  console.log(decodedJson)
+                } catch(error) {
+                  console.log(error)
+                  return;
+                }
+              },
+              500
+            );
+
+            setWasntSettedInterval(false);
+          }
+        }}
+        >
+          ジョイスティックと接続
+        </Button>
+      }
       <p>wasdか十字キーでも操作できます</p>
       <NextReactP5Wrapper sketch={p5 => sketch(p5, xVal)} />
     </main>
